79da9949c7210ce989f381b470e5e0d7ae75dc7b
bernadinho123 <48894522+bernadinho123@users.noreply.github.com>
2023-06-18 21:08:46 -0300

    Update gerestoque.py

--- modulos/grupoG/gerestoque.py
+++ modulos/grupoG/gerestoque.py
@@ -1,3 +1,5 @@
+#GerEstoque
+
 #Função para verificar se o código é válido
 def cod_valido(num):
     soma = 0
@@ -7,12 +9,14 @@ def cod_valido(num):
     if 30 < soma < 100:
         return 1 #O código é válido
     else:
-        return 0 #O código é inválido
-
+        return 0 #O código é inválido  
 #Escolha de opção
 
-operacao=input('Digite (i) para incluir um item, (e) para excluir e (s) para sair. ')
-listaEstoque=[]
+operacao=input('Digite (i) para incluir um item, (e) para excluir e (a)para alterar um item(s) para sair.\n ')
+listasaldo=[]
+listacod=[]
+listaval=[]
+listadesc=[]
 abacate=True
 cont=0
 i=0
@@ -29,7 +33,7 @@ while abacate==True:
       
       qtd_itens=int(input('Digite quantos itens, no total, serão inclusos: \n'))
       total_itens+=qtd_itens
-      listasaldo=['o']*qtd_itens
+      
       
       #Loop para a quantidade de itens
       for i in range(qtd_itens): 
@@ -38,7 +42,6 @@ while abacate==True:
         flag=True
         while flag==True:
           cod_item=int(input('Digite o código do item: \n'))
-        
           if cod_valido(cod_item)==0: #Código inválido
             print('Código inválido.\nA soma dos digitos deve ser entre 30 e 100.\n')
             cod_item=0 
@@ -46,15 +49,14 @@ while abacate==True:
           elif cod_valido(cod_item)==1: #Código válido
             flag=False
 
-        
+        listacod.append(str(cod_item))
         saldo_item=int(input('Digite a quantidade desse item que será incluso: \n'))
+        listasaldo.append(saldo_item)
         desc_item=input('Digite a descrição do item: \n')
+        listadesc.append(desc_item)
         valor_item=float(input('Digite o valor do item, em reais: \n'))
-        
-      
-        listasaldo[cont]=saldo_item
-        cont+=1 
-        
+        listaval.append(valor_item)
+    
         #Calculando a média
         total_valor+=valor_item
         media=total_valor/total_itens
@@ -65,32 +67,31 @@ while abacate==True:
           mcodigo=cod_item
 
         #Atualizando a listaEstoque
-        item = {'Código': cod_item, 'Descrição': desc_item, 'Valor': valor_item, 'Saldo': saldo_item}
-        listaEstoque.append(item)
-        
+       
         
       #Printando
-      print('---------------------------------------------------')
-      print('A média dos valores dos itens cadastrados é: ',media,'\n')
-      print('O maior valor posto é do item de código: ',mcodigo,'e vale: ', mvalor)
-      print('---------------------------------------------------')
-      operacao=input('Digite (i) para incluir um item, (e) para excluir e (s) para sair. \n')
+      print('------------------------------\n')
+      print('valor medio dos itens cadastrados é: ',media,'\n')
+      print('Item de maior valor cadastrado é: ',mcodigo,'e vale: ', mvalor,'\n')
+      print('------------------------------\n')
+      operacao=input('Digite (i) para incluir um item, (e) para excluir,(a) para alterar um item ou (0) para sair. \n')
   
   #Exclusão
     case 'e':
-      codigos_excluidos=[]
       sopa=True
+      
       while sopa==True: 
-        cod_excl=int(input('Digite o código a ser excluído: \n'))
+        cod_excl=input('Digite o código a ser excluído: \n')
+        
       
-      
-        if cod_valido(cod_excl)==0:
+        if cod_excl not in listacod:
           print('O código é inválido. Transação encerrada.\n') #Verificando se o código é válido
           sopa=False
           abacate=False
 
         else:
-        
+          indice=listacod.index(cod_excl)
+          
           alerta=input('Tem certeza que deseja excluir o item? S ou N: \n') #Alerta
         
           match alerta:
@@ -98,54 +99,60 @@ while abacate==True:
             case'S': #Usuário escolhe sim
             
               itens_excl=int(input('Digite quantos itens devem ser excluídos: \n'))
-            
-              item_encontrado=False
-            
-              for item in listaEstoque:
-                        if item['Código'] == cod_excl:
-                            item_encontrado = True
-                            qtd_retirada = min(item['Saldo'], itens_excl)
-                            item['Saldo'] -= qtd_retirada
-                            listasaldo[listaEstoque.index(item)] -= qtd_retirada
-                            codigos_excluidos.append((cod_excl,'         ', qtd_retirada,)) 
-                            if item['Saldo']==0:
-                                listaEstoque.remove(item)
-                            break
-
-              if item_encontrado==False:
-                print ('O código é inválido pois não há saldo. Operação encerrada.\n')
-                abacate=False
+              listasaldo[indice]-=itens_excl
+  
+              
+                  
+                  
+                    
+                        
+              if listasaldo[indice]<0:
+                print ('O código é inválido pois não há saldo suficiente. Operação encerrada.\n')
+                listasaldo[indice]+=itens_excl
+              
                 sopa=False
+                operacao=input('Digite (i) para incluir um item, (e) para excluir,(a) para alterar um item ou (0) para sair. \n')
               else:
-                print('Operação relizada com sucesso: ', listaEstoque,'\n')
+                print('------------------------------------\n')
+                print('Relatorio de itens excluídos\n')
+                print('item     saldo')
+                for i in range (total_itens):
+                  print(listacod[i],'     ',listasaldo[i],'\n')
+                print('------------------------------------\n')
                 desejo=input('Deseja excluir outro item? S ou N: \n')
                 if desejo=='N':
-                  abacate=False
+                  
                   sopa=False
                   print('Transação encerrada.\n')
-                  print('Após as operações, o saldo do estoque está assim: ', listaEstoque,'\n')
-                  
-                  print('---------------------------------------')
-                  print('RELATÓRIO DE ITENS EXCLUÍDOS \n')
-                  print(' ITEM          SALDO RETIRADO \n')
-                  for cod_excl in codigos_excluidos:
-                    print(cod_excl)
-                    print('\n')
-                  print('----------------------------------------')
+                  operacao=input('Digite (i) para incluir um item, (e) para excluir,(a) para alterar um item ou (0) para sair. \n')
+                  sopa=False
+                
+              
             
+          
             case'N': #Usuário escolhe não
-              
-              
-              
               print('Operação encerrada.\n')
-              
-              abacate=False
+             
+              operacao=input('Digite (i) para incluir um item, (e) para excluir,(a) para alterar um item ou (0) para sair. \n')
+          
               sopa=False
               
-                           
-
-
-  
+    
     case 's':
       print('Saiu do programa.\n')
       abacate=False
+    case 'a':
+      altera=0
+      altera=input('digite o codigo q deseja alterar: ')
+      if altera not in listacod:
+        print('codigo invalido')
+      else:  
+        indice=listacod.index(altera)
+        listasaldo[indice]=int(input('digite o novo saldo: \n'))
+        listaval[indice]=int(input('digite o novo valor: \n'))
+        listadesc[indice]=input('digite a nova descricao: \n')
+        print('------------------------------------------------')
+        print('Item          Descrição          Valor         Saldo')
+        print(listacod[indice],'      ',listadesc[indice],'              ',listaval[indice],'                 ',listasaldo[indice])
+        print('------------------------------------------------\n\n') 
+        operacao=input('Digite (i) para incluir um item, (e) para excluir,(a) para alterar um item ou (0) para sair. \n')
