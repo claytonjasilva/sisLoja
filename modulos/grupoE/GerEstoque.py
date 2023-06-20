e1f6ba4c28bbac30d0f75d6a0797ca3430171dce
GuilhermeVA <133550414+GuilhermeVA@users.noreply.github.com>
2023-06-19 21:42:16 -0300

    Update GerEstoque.py

--- modulos/grupoE/GerEstoque.py
+++ modulos/grupoE/GerEstoque.py
@@ -4,12 +4,24 @@
 #Pedro Henrique Abdalla Ramos / 202304077754: TA
 #Victor Alvarenga Hwang / 202208766005: TA
 #Gabriel Mendonça de Medeiros / 202302855458: TA
-#Gabriela Borsoi Cohen / 202208431021: TA
+#Gabriela Borsoi Cohen / 202208431021: TP
 
+def soma_digito(n):
+  soma = 0
+  while n >= 1:
+    digito = n % 10 
+    n = n // 10
+    soma = soma + digito
+  return soma
+
+def codigo_invalido():
+  print("------------------")
+  print("Código inválido")
+  print("------------------")
 
 
 #escolha da opção pelo usuário
-ListaEstoque = []
+ListaEstoque = dict()
 while True:
   print("----------------------------")
   print("Sistema de Gestão da SisLoja")
@@ -20,7 +32,8 @@ while True:
   match opcao:
     case "1":                             #GerEstoque
       print("\n----------------------------")
-      op = input("Escolha a operação: \n1) Inclusão de itens \n2) Exclusão de itens \n----------------------------\n")
+      op = input("Escolha a operação: \n1) Inclusão de itens \n2) Exclusão de itens \n3) Alteração de itens \n----------------------------\n")
+      
       match op:
         case "1":                        #Inclusão
           soma_valores = 0               #variavel que soma os valores dos itens
@@ -38,6 +51,9 @@ while True:
             print('------------------------------------')
             print('\n')
             cod_item=int(input("Digite o código do item: "))
+            if soma_digito(cod_item) < 30 or soma_digito(cod_item) > 100:
+              codigo_invalido()
+              continue
             print('\n')
             desc_item=input("Digite o nome do item: ")
             print('\n')
@@ -46,17 +62,12 @@ while True:
             valor_item=float(input("Digite o valor do item: "))
             print('------------------------------------')
              # Verifica se o item já está na lista e, em caso afirmativo, soma as unidades
-            encontrado = False
-            for item in ListaEstoque:
-              if item[0] == cod_item:
-                item[3] += unidades
-                encontrado = True
-                break
+            if str(cod_item) in ListaEstoque.keys():
+              ListaEstoque[str(cod_item)][2] = ListaEstoque[str(cod_item)][2] + unidades
 
             #Se o item não foi encontrado na lista, adiciona-o
-            if not encontrado:
-              item_info = [cod_item, desc_item, valor_item, unidades]
-              ListaEstoque.append(item_info)
+            else:
+              ListaEstoque[str(cod_item)] = [desc_item, valor_item, unidades]
             #soma os valores dos itens
             soma_valores = soma_valores + valor_item*unidades   
             cont = cont + unidades
@@ -67,14 +78,18 @@ while True:
               maior_cod = cod_item
           
           #Exibe a lista preenchida pelo usuário
-          print("Relatorio:")
-          media_valores = soma_valores / cont
-          print("------------------------------------------------")
-          print("Média dos valores dos itens cadastrados:R$",media_valores)
-          print("Código do item de maior valor: ",maior_cod,", Valor:R$",maior_valor)
-          print("------------------------------------------------")
-          print("Teclar 0 para voltar para a tela principal\n ")
-          x=input()
+          if soma_valores != 0 or cont != 0:    #caso não tenha incluido nada vai dar erro de divisão por zero sem essa linha
+            print("Relatorio:")
+            media_valores = soma_valores / cont
+            print("------------------------------------------------")
+            print("Média dos valores dos itens cadastrados:R$",media_valores)
+            print("Código do item de maior valor: ",maior_cod,", Valor:R$",maior_valor)
+            print("------------------------------------------------")
+            print("Teclar 0 para voltar para a tela principal\n ")
+            x=input()
+
+
+      
         #exclusão  
         case "2":
           ListaExcluidos = []        #lista dos itens que foram excluidos
@@ -84,67 +99,37 @@ while True:
             print('\n')
             item_excluir = int(input("Digite o código do item que deseja excluir: "))
             print('\n')
-            indice = 0                  #indice da lista estoque 
             soma = 0                    #variavel para verificar o codigo
             saldo_suficiente=False
-            for digito in str(item_excluir):
-              soma += int(digito)
-            #valida o codigo segundo a definição dada no arquivo do trabalho
-            if soma>30 and soma< 100:
+            
+            if soma_digito(item_excluir) < 30 or soma_digito(item_excluir) > 100:
+              codigo_invalido()
+              break
               # verifica se o código do item está na lista de estoque
-              if not any(item_excluir == item[0] for item in ListaEstoque):
-                print("------------------")
-                print("Código inválido")
-                print("------------------")
+            if str(item_excluir) not in ListaEstoque.keys():
+                codigo_invalido()
                 break
               #qntd a excluir de unidades
-              qtd_excluir=int(input("Quantas unidades desse item devem ser excluidas?: "))
-              print('\n')
-              while indice < len(ListaEstoque):
-                #verifica se o item esta na lista
-                if item_excluir == ListaEstoque[indice][0]:
+            qtd_excluir=int(input("Quantas unidades desse item devem ser excluidas?: "))
+            print('\n')
                   #confirmação para excluir o item
-                  excluir = input("Tem certeza que deseja excluir (sim) (nao) \n")
-                  print('\n')
-                  if excluir == "sim":
+            excluir = input("Tem certeza que deseja excluir (sim) (nao) \n")
+            print('\n')
+            
+            if excluir == "sim":
                     #tomar a ação com base no numero de unidades que serão excluidas
-                      if ListaEstoque[indice][3] < qtd_excluir:
+                      if ListaEstoque[str(item_excluir)][2] < qtd_excluir:
                         print("Saldo insuficiente!. Transação encerrada.")
                         break
-                      ListaEstoque[indice][3]-=qtd_excluir
+                      ListaEstoque[str(item_excluir)][2]-=qtd_excluir
                       print("OPERAÇÃO REALIZADA COM SUCESSO")
                     #itens excluidos serão adicionados à ListaExcluidos
                       info_exclusao = [item_excluir, qtd_excluir]
                       ListaExcluidos.append(info_exclusao)
-                      if ListaEstoque[indice][3] != 0:
-                        print("Itens restantes: ",ListaEstoque)
-                        break
-                      if ListaEstoque[indice][3]==0: 
-                        saldo_suficiente=True
-                        ListaEstoque.remove(ListaEstoque[indice])
-                        print("Itens restantes: ",ListaEstoque)
-                        break
-                  elif excluir == "nao":
+            elif excluir == "nao":
                     print("Transação encerrada")
                     break
-                elif indice < len(ListaEstoque):
-                  indice = indice + 1
-                  continue
-                else:
-                  #caso o codigo digitado nao esteja na lista
-                  indice=-1
-                indice=indice+1
-              if indice == len(ListaEstoque) and len(ListaEstoque) == 0 and not saldo_suficiente or indice==-1:
-                print("------------------")
-                print("Código inválido")
-                print("------------------")
-                break
-            else:
-              #caso a soma dos digitos nao esteja entre 30 e 100
-              print("------------------")
-              print("Código inválido")
-              print("------------------")
-              break
+              
             print('\n')  
             continuar = input("Deseja excluir outro item? (sim) (nao) \n")
             if continuar == "sim":
@@ -162,3 +147,28 @@ while True:
             print("{:<10} {:<10}".format(item_excluir[0],       item_excluir[1]))
             print("------------------------------------------------")
           input("Digite 0 para retornar ao menu\n")
+
+
+      
+        case "3":
+          while True:
+            print(ListaEstoque)
+            item_alterar = input("Digite o código do item que deseja alterar: ")
+            if str(item_alterar) not in ListaEstoque.keys():
+              codigo_invalido()
+              break
+            desc_item=input("Digite o novo nome do item: ")
+            print('\n')
+            unidades=int(input("Digite a novo valor de unidades: "))
+            print('\n')
+            valor_item=float(input("Digite o novo valor do item: "))
+            ListaEstoque[item_alterar] = [desc_item, valor_item, unidades]
+            print("Alteração efetuada com sucesso.")
+            print(ListaEstoque)
+            print('\n')
+            print("----------------------------")
+            print("Relatorio de item alterado")
+            print("{:<10} {:<10}   {:<10} {:<10}".format("ITEM","DESCRICAO","VALOR","SALDO"))
+            print("{:<10} {:<10}   {:<10} {:<10}".format(item_alterar,       ListaEstoque[item_alterar][0],       ListaEstoque[item_alterar][1],       ListaEstoque[item_alterar][2]))
+            print("-----------------------------")
+            break
